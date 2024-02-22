0bde2cbfcba74fe6f50a32f8f3c2fd72c2b08a44
burning23185 <58378530+burning23185@users.noreply.github.com>
2024-02-22 14:53:46 +0900

    Time: 44 ms (56.08%), Space: 16.6 MB (68.09%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,14 +1,44 @@
 class Solution:
+    import sys
+
+class Solution:
+    def _merge(self, arr1, arr2):
+        result = []
+        i = j = 0
+
+        while i < len(arr1) and j < len(arr2):
+            str_i = str(arr1[i])
+            str_j = str(arr2[j])
+
+            if str_i + str_j > str_j + str_i:
+                result.append(arr1[i])
+                i += 1
+            else:
+                result.append(arr2[j])
+                j += 1
+
+        while i < len(arr1):
+            result.append(arr1[i])
+            i += 1
+
+        while j < len(arr2):
+            result.append(arr2[j])
+            j += 1
+
+        return result
     
+    def mergeSort(self, arr):
+        if len(arr) <= 1:
+            return arr
+        
+        pivot = len(arr)//2
+
+        L = arr[:pivot]
+        R = arr[pivot:]
+
+        return self._merge(self.mergeSort(L), self.mergeSort(R))
+
+
     def largestNumber(self, nums: List[int]) -> str:
-
-        for i in range(0, len(nums)):
-            for j in range(i, len(nums)):
-                str_i = str(nums[i])
-                str_j = str(nums[j])
-
-                if str_i + str_j < str_j + str_i:
-                    nums[i], nums[j] = nums[j], nums[i]
-                    continue
-                
-        return '0' if sum(nums) == 0 else ''.join(str(n) for n in nums)
+        sorted = self.mergeSort(nums)
+        return '0' if sum(nums) == 0 else ''.join(str(n) for n in sorted)
